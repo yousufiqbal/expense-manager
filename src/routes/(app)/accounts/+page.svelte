142e71b9084bc5b672ea87124b5646efd6322994
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-11-21 23:17:51 +0500

    ..

--- src/routes/(app)/accounts/+page.svelte
+++ src/routes/(app)/accounts/+page.svelte
@@ -1,7 +1,11 @@
 <script>
   import Button from "$lib/components/Button.svelte";
+  import Buttons from "$lib/components/Buttons.svelte";
   import Title from "$lib/components/Title.svelte";
 </script>
 
 <Title title="Accounts" icon="ri:folders-line" />
-<Button name="New Account" href="/accounts/add-account" icon="ri:add-line" />
+
+<Buttons>
+  <Button name="New Account" href="/accounts/add-account" icon="ri:add-line" />
+</Buttons>
