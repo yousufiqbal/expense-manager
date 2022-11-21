ace959fbcdd0352ea246393c19d49fd4ccc9fa21
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-11-21 13:27:43 +0500

    ..

---
+++ src/lib/components/Bar.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Icon from '@iconify/svelte'
+</script>
+
+<div class="bar">
+  <a href="/">
+    <i><Icon /></i>
+    <span>Home</span>
+  </a>
+  <a href="/">
+    <i><Icon /></i>
+    <span>Stats</span>
+  </a>
+  <a href="/">
+    <i><Icon /></i>
+    <span>Search</span>
+  </a>
+  <a href="/">
+    <i><Icon /></i>
+    <span>Accounts</span>
+  </a>
+  <a href="/">
+    <i><Icon /></i>
+    <span>Settings</span>
+  </a>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,3 @@
+<script>
+  import '$lib/styles/all.css'
+</script>
