04e19b0f7beed903759bde4b8b5d191d263946dd
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-11-21 17:13:39 +0500

    ..

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -3,13 +3,13 @@
     import Title from "$lib/components/Title.svelte";
 
   let items = [
-    { name: 'Categories', href: '/settings/profile', icon: 'bx:category' },
-    { name: 'Account Groups', href: '/settings/profile', icon: 'clarity:blocks-group-line' },
-    { name: 'Backups', href: '/settings/profile', icon: 'ri:database-2-line' },
-    { name: 'Currency', href: '/settings/profile', icon: 'ri:currency-line' },
+    { name: 'Categories', href: '/settings/categories', icon: 'bx:category' },
+    { name: 'Account Groups', href: '/settings/account-groups', icon: 'clarity:blocks-group-line' },
+    { name: 'Backups', href: '/settings/backups', icon: 'ri:database-2-line' },
+    { name: 'Currency', href: '/settings/currency', icon: 'ri:currency-line' },
     { name: 'Profile', href: '/settings/profile', icon: 'ri:profile-line' },
-    { name: 'Theme', href: '/settings/profile', icon: 'ri:sun-foggy-line' },
-    { name: 'About', href: '/settings/profile', icon: 'ri:information-line' }
+    { name: 'Theme', href: '/settings/theme', icon: 'ri:sun-foggy-line' },
+    { name: 'About', href: '/settings/about', icon: 'ri:information-line' }
   ]
 </script>
 
