4847d1ca5893768b002b15695fafffcb74d257e7
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-11-24 13:41:12 +0500

    ..

--- src/routes/(app)/accounts/[mode]-account/+server.js
+++ src/routes/(app)/accounts/[mode]-account/+server.js
@@ -4,7 +4,7 @@ import { error, json } from '@sveltejs/kit'
 import dayjs from 'dayjs'
 
 /** @type {import('./$types').RequestHandler} */
-export const POST = async ({ request, cookies }) => {
+export const POST = async ({ request, cookies, locals }) => {
 
   const body = await request.json()
   const account = await accountSchema.validate(body, { abortEarly: false })
@@ -12,6 +12,7 @@ export const POST = async ({ request, cookies }) => {
   // Adding..
   const result = await db.insertInto('accounts')
     .values({
+      userId: locals.userId,
       name: account.name
     }).executeTakeFirst()
 
@@ -20,6 +21,7 @@ export const POST = async ({ request, cookies }) => {
   // If has starting balance.. adding income as 'starting balance'
   await db.insertInto('incomes')
     .values({
+      userId: locals.userId,
       accountId,
       amount: account.balance,
       date: dayjs().format('YYYY-MM-DD'),
@@ -35,7 +37,7 @@ export const POST = async ({ request, cookies }) => {
 }
 
 /** @type {import('./$types').RequestHandler} */
-export const PUT = async ({ request, url }) => {
+export const PUT = async ({ request, url, locals }) => {
 
   const accountId = await url.searchParams.get('account-id')
   const body = await request.json()
@@ -44,16 +46,18 @@ export const PUT = async ({ request, url }) => {
   // Check Duplicate
   const result = await db.selectFrom('accounts')
     .where('accounts.name', '=', account.name)
+    .where('accounts.userId', '=', locals.userId)
     .selectAll().executeTakeFirst()
-  
-  if (result) throw error(400, 'Account with this name already exist. Use different name.')
-
-  // Adding..
-  await db.updateTable('accounts')
+    
+    if (result) throw error(400, 'Account with this name already exist. Use different name.')
+    
+    // Adding..
+    await db.updateTable('accounts')
     .set({
       name: account.name
     })
     .where('accounts.accountId', '=', accountId)
+    .where('accounts.userId', '=', locals.userId)
     .executeTakeFirst()
 
   return json({
--- src/routes/(app)/settings/categories/[mode]-category/+server.js
+++ src/routes/(app)/settings/categories/[mode]-category/+server.js
@@ -3,12 +3,17 @@ import { db } from '$lib/server/db'
 import { json } from '@sveltejs/kit'
 
 /** @type {import('./$types').RequestHandler} */
-export const POST = async ({ request }) => {
+export const POST = async ({ request, locals }) => {
 
   const body = await request.json()
   const category = await categorySchema.validate(body, { abortEarly: false })
 
-  await db.insertInto('categories').values(category).execute()
+  await db.insertInto('categories')
+    .values({
+      ...category,
+      userId: locals.userId
+    })
+    .execute()
   
   return json({
     message: 'Category added'
@@ -16,7 +21,7 @@ export const POST = async ({ request }) => {
 }
 
 /** @type {import('./$types').RequestHandler} */
-export const PUT = async ({ request, url }) => {
+export const PUT = async ({ request, url, locals }) => {
 
   const categoryId = url.searchParams.get('category-id')
   const body = await request.json()
@@ -24,6 +29,7 @@ export const PUT = async ({ request, url }) => {
 
   await db.updateTable('categories')
     .where('categories.categoryId', '=', categoryId)
+    .where('categories.userId', '=', locals.userId)
     .set(category).execute()
   
   return json({
