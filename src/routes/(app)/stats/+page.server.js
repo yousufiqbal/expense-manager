326e3146d98bbc005c005acb2e8c02508f043dd0
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-11-27 19:53:54 +0500

    setQuery fixed

--- src/routes/(app)/stats/+page.server.js
+++ src/routes/(app)/stats/+page.server.js
@@ -11,56 +11,41 @@ export const load = async ({ locals, url }) => {
   let start = url.searchParams.get('start') || dayjs().startOf('month').format('YYYY-MM-DD')
   let end = url.searchParams.get('end') || dayjs().endOf('month').format('YYYY-MM-DD');
 
-  let categories = []
+  let stats = [];
 
-  if (tab == 'expense') {
+  // ${accountIds.length != 0 ? 'AND e.accountId IN' + sql.join(accountIds) : ''}
+  // console.log(accountIds)
 
-
-    categories = (await sql`
-      WITH resultSet AS (
-        SELECT ec.expenseCategoryId, ec.name, COALESCE(SUM(e.amount), 0) AS total
-        FROM expense_categories ec
-        LEFT JOIN expenses e ON e.expenseCategoryId = ec.expenseCategoryId
-        WHERE ec.userId = ${locals.userId}
-        AND e.date BETWEEN ${start} AND ${end}
-        GROUP BY ec.expenseCategoryId
+  let query = `
+    WITH result AS (
+      SELECT *
+      FROM expenses e
+      WHERE e.userId = ${locals.userId}
+      AND e.date BETWEEN ${start} AND ${end}
+      GROUP BY e.expenseCategoryId
       )
       
-      SELECT ec.name, coalesce(total, 0) as total
+      SELECT ec.name, COALESCE(result.amount, 0) AS total
       FROM expense_categories ec
-      LEFT JOIN resultSet ON resultSet.expenseCategoryId = ec.expenseCategoryId
-      WHERE ec.userId = ${locals.userId}
-      ORDER by total desc
-
-    `.execute(db)).rows
+    LEFT JOIN result ON result.expenseCategoryId = ec.expenseCategoryId
     
-
-    // -- AND e.accountId IN (${sql.join(accountIds)})
-  }
-  let transfer 
-  if (tab == 'expense') {
-
-    transfer = await db.selectFrom('transfers')
-      .where('transfers.userId', '=', locals.userId)
-      .where('transfers.date', '>=', start)
-      .where('transfers.date', '<=', end)
-      .if(accountIds?.length != 0, qb => qb.where('transfers.fromAccountId', 'in', accountIds))
-      .groupBy('transfers.userId')
-      .select(coalesce(db.fn.sum('transfers.amount'), 0).as('total'))
-      .executeTakeFirst()
+    UNION ALL 
     
-  }
+    SELECT 'Transfer' NAME, COALESCE(t.amount, 0) AS total
+    FROM transfers t
+    WHERE t.userId = ${locals.userId}
+    AND t.date BETWEEN ${start} AND ${end}
+    GROUP BY t.transferId
+  `
 
-  let accounts = await db.selectFrom('accounts')
+  const result = (await sql(query).execute(db)).rows
+
+  console.log(result)
+
+  const accounts = await db.selectFrom('accounts')
     .where('accounts.userId', '=', locals.userId)
     .selectAll().execute()
 
-  categories.push({ name: 'Transfer', total: transfer?.total || 0 })
-
-  const allTotal = categories.map(c => c.total)?.reduce((a, b) => +a + +b, 0) + +transfer?.total
-
-  console.log(allTotal)
-
-  return { accounts, categories, transfer, allTotal }
+  return { accounts, stats }
 
 }
