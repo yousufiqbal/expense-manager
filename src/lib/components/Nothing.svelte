a60a3b81dded1ef100c8b7dabfcae72b0655140c
Yousuf Iqbal <yousufiqbalhashim@gmail.com>
2022-11-29 10:34:41 +0500

    ..

--- src/lib/components/Nothing.svelte
+++ src/lib/components/Nothing.svelte
@@ -1,9 +1,19 @@
+<script>
+
+    import Icon from "@iconify/svelte";
+
+</script>
 <div class="nothing">
+  <i><Icon icon="ri:information-line" /></i>
   <slot></slot>
 </div>
 
-<!-- <style>
+<style>
   .nothing {
-    /* border: 1px dashed red; */
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding-bottom: 10px;
+    border-bottom: 1px dashed var(--red);
   }
-</style> -->
+</style>
